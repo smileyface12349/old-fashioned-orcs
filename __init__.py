b0a3322b5757b68f7f9e1e61097a89e5c7c58f58
fortwoone <74463377+fortwoone@users.noreply.github.com>
2022-07-22 11:45:10 +0200

    Is Lint gonna be happy this time? Please tell me it is (again)

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ running = True
 while running:
     # We generally use a while loop when making a game. Most of the game code should go here.
     screen.fill("skyblue")
-    pygame.display.update() # This function is called when everything render-related is done.
+    pygame.display.update()  # This function is called when everything render-related is done.
     # If you don't call this or pygame.display.flip, the screen won't show what you've drawn on it!
     # Events are how we manage player inputs (and others).
     for event in pygame.event.get():
