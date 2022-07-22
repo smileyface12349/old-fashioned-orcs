27358adb3de4ddd260ecf0e2b56290471979499e
fortwoone <74463377+fortwoone@users.noreply.github.com>
2022-07-22 11:34:19 +0200

    Is Lint gonna be happy this time?

--- src/solid.py
+++ src/solid.py
@@ -1,4 +1,8 @@
 import pygame
 
 class Solid(pygame.sprite.Sprite):
-    pass
+    def __init__(self, tile_pos:tuple):
+        super().__init__()
+        self.tile_pos=tile_pos #tile_pos is a tuple of integers representing a tile's topleft corner coordinates
+        #We'll only need to multiply these coords by 16 to have the real position
+
