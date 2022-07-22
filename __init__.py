2a08230872f72a519f7e376fbac548e8044c1b48
fortwoone <74463377+fortwoone@users.noreply.github.com>
2022-07-22 13:06:02 +0200

    Game engine begun (player collisions, WIP) : reformatted by Black

--- __init__.py
+++ __init__.py
@@ -9,15 +9,15 @@ pygame.init()  # ensuring that everything we need will be initialised before sta
 screen = pygame.display.set_mode((160, 144), pygame.RESIZABLE | pygame.SCALED)
 
 game = src.game.Game()
-clock=pygame.time.Clock()  # a framerate helper object.
+clock = pygame.time.Clock()  # a framerate helper object.
 
 running = True
 
 while running:
     # We generally use a while loop when making a game. Most of the game code should go here.
     screen.fill("skyblue")
-    dt=clock.tick(60)  # this ensures that the game cannot run higher that 60FPS. We also get a delta time in ms.
-    game.objects.update(dt)  #Auto update for every sprite
+    dt = clock.tick(60)  # this ensures that the game cannot run higher that 60FPS. We also get a delta time in ms.
+    game.objects.update(dt)  # Auto update for every sprite
     game.objects.draw(screen)  # We draw everything here
     pygame.display.update()  # This function is called when everything render-related is done.
     # If you don't call this or pygame.display.flip, the screen won't show what you've drawn on it!
@@ -28,16 +28,16 @@ while running:
             # Generally we just close the window when we get an event of that type.
             running = False
             pygame.quit()
-        elif event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_LEFT:
-                game.player.moving_left=True
-            elif event.key==pygame.K_RIGHT:
-                game.player.moving_right=True
-        elif event.type==pygame.KEYUP:
-            if event.key==pygame.K_LEFT:
-                game.player.moving_left=False
-            elif event.key==pygame.K_RIGHT:
-                game.player.moving_right=False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.player.moving_left = True
+            elif event.key == pygame.K_RIGHT:
+                game.player.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                game.player.moving_left = False
+            elif event.key == pygame.K_RIGHT:
+                game.player.moving_right = False
 
 
 try:
--- src/player.py
+++ src/player.py
@@ -10,7 +10,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, game):
         super().__init__()  # we need this to ensure the sprite will work correctly with groups
         # This is the game object. We need it for collisions.
-        self.game=game
+        self.game = game
         # Surfaces are image objects. We can replace this with assets once they're made
         self.image = pygame.Surface((16, 16)).convert_alpha()
         # convert_alpha is a method to allow us to use PNG images with transparency, and also make them faster to
@@ -23,10 +23,11 @@ class Player(pygame.sprite.Sprite):
         # And so adding 1 to self.rect.y will move self.rect 1 pixel downwards.
         self.x_velocity = 1
         self.y_velocity = 0
-        self.falling=True
-        self.fall_delay=0
-        self.moving_right=False
-        self.moving_left=False
+        self.falling = True
+        self.fall_delay = 0
+        self.moving_right = False
+        self.moving_left = False
+
     # The two methods below move the player.
     def move_left(self):
         """Moves the Player left"""
@@ -34,20 +35,25 @@ class Player(pygame.sprite.Sprite):
 
     def move_right(self):
         """Moves the Player right"""
-        self.rect.x+=self.x_velocity
+        self.rect.x += self.x_velocity
+
     def update(self, dt):
         if self.moving_left:
             self.move_left()
         if self.moving_right:
             self.move_right()
-        self.fall_sensor.midtop=self.rect.midbottom
+        self.fall_sensor.midtop = self.rect.midbottom
         # Start falling only if there's no solid tile underneath the player and being on the same layer
-        if (not self.falling) and \
-                (not pygame.sprite.spritecollide(self, self.game.tiles, False,
-                                                lambda spr1, spr2:spr1.fall_sensor.colliderect(spr2.rect) and \
-                                                 not self.game.objects.get_layer_of_sprite(spr2))):
-            self.falling=True
-            self.y_velocity=1
+        if (not self.falling) and (
+            not pygame.sprite.spritecollide(
+                self,
+                self.game.tiles,
+                False,
+                lambda spr1, spr2: spr1.fall_sensor.colliderect(spr2.rect)
+                and not self.game.objects.get_layer_of_sprite(spr2),
+            )
+        ):
+            self.falling = True
+            self.y_velocity = 1
         elif self.falling:
             pass
-
