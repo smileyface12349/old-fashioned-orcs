7a3d7e6d7cc726425c011bbbc72cfb70f3e96b3b
Paulchen <lukas.23022005@gmail.com>
2022-07-24 18:52:12 +0200

    file to create custom player images

---
+++ src/playerimage.py
@@ -0,0 +1,23 @@
+from time import sleep
+from PIL import Image
+
+
+class PlayerImage:
+    def __init__(self, color_tuple): #color tuple in the following format: (r, g, b, 255)
+        self.color = color_tuple #tuple containing new color for the plyer image
+
+    def newimage(self):
+        img = Image.open("old-fashioned-orcs\\assets\player_base.png") #path to open image
+        img = img.convert("RGBA") #convert image to RGBA format
+
+        w, h = img.size #width, height of the image
+        data = img.getdata() #color tuples of image pixels in a list
+        newdata = [] #empty list for new color tuples of image pixels
+        for i in range(w*h):
+            if data[i] == (0, 255, 0, 255): #if pixel color is green
+                newdata.append(self.color)
+            else:
+                newdata.append(data[i])
+                
+        img.putdata(newdata) #create new player image out of given data
+        img.save("old-fashioned-orcs\\assets\player.png") #path where the new image get saved
