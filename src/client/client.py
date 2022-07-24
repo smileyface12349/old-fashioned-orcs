09a773de9fbb47b9bb1aa813fb696932af38fffd
Phil <80522768+Nasty07@users.noreply.github.com>
2022-07-24 17:35:09 +0300

    Turned client-side into a class

--- src/client/client.py
+++ src/client/client.py
@@ -7,74 +7,82 @@ from cache import CacheManager
 cache = CacheManager()
 
 
-async def hello(websocket, cache_data):
-    """Typical client/server hello connection"""
-    no_cache = True if not all(cache_data.values()) else False
+class Client:
+    """Client class that handles the connection with the server."""
 
-    hi = json.dumps(cache_data)
-    await websocket.send(hi)
-    print(f"Sending => {hi}")
+    def __init__(self):
+        self.websocket = None
+        self.payload = {}
 
-    response = await websocket.recv()
-    response = json.loads(response)
-    print(f"Response => {response}")
+    def sync_game(self):
+        """Sync real game data to send back to the server!"""
+        payload = {"type": "play", "position": [0, 0], "level": -1}
+        # TODO => create the real communication with the engine
+        return payload
 
-    if response["type"] in ["init", "ready"]:
+    async def _hello(self, cache_data):
+        """Typical client/server hello connection"""
+        no_cache = True if not all(cache_data.values()) else False
 
-        if not cache_data["unique_id"]:
-            # If user didnt have a unique_id, server returned him one
-            cache_data["unique_id"] = response["unique_id"]
+        hi = json.dumps(cache_data)
+        await self.websocket.send(hi)
+        print(f"Sending => {hi}")
 
-        if not cache_data["nickname"]:
-            # If user didnt specify a nickname, server returns a random one
-            cache_data["nickname"] = response["nickname"]
-
-        if no_cache:
-            # If there is no cache saved, we need to create it
-            await cache.save(response)
-            no_cache = False
-        return cache_data
-
-    else:
-        raise Exception("Invalid type in response")
-
-
-async def play(websocket, payload):
-    """TODO => actually play"""
-    payload["type"] = "play"
-
-    for _ in range(1000):
-        # Update the payload with real data
-        print("=> Playing..")
-
-        payload["position"] = [0, 0]
-        payload["level"] = -1
-
-        # When ready send it back to the server!
-        await websocket.send(json.dumps(payload))
-        print(f"Sending => {payload}")
-
-        # Wait for the response/update and process it
-        response = await websocket.recv()
+        response = await self.websocket.recv()
         response = json.loads(response)
         print(f"Response => {response}")
-        await asyncio.sleep(0.1)
 
+        if response["type"] in ["init", "ready"]:
 
-async def main():
-    """Typical client connection"""
-    cache_data = await cache.load()
+            if not cache_data["unique_id"]:
+                # If user didnt have a unique_id, server returned him one
+                cache_data["unique_id"] = response["unique_id"]
 
-    # Temporary fix for the nickname
-    if not cache_data["nickname"]:
-        cache_data["nickname"] = input("Enter a nickname: ")
+            if not cache_data["nickname"]:
+                # If user didnt specify a nickname, server returns a random one
+                cache_data["nickname"] = response["nickname"]
 
-    async with websockets.connect("ws://localhost:8000/") as websocket:
-        # Send the first data to initialize the connection
-        payload = await hello(websocket, cache_data)
-        # Now play the game
-        await play(websocket, payload)
+            if no_cache:
+                # If there is no cache saved, we need to create it
+                await cache.save(response)
+                no_cache = False
+            return cache_data
+        else:
+            raise Exception("Invalid type in response")
+
+    async def play(self, payload):
+        """TODO => actually play"""
+        print("=> Playing..")
+        while True:
+            loop = asyncio.get_event_loop()
+            payload = await loop.run_in_executor(None, self.sync_game)
+            self.payload.update(payload)
+
+            # When ready send it back to the server!
+            await self.websocket.send(json.dumps(self.payload))
+            print(f"Sending => {self.payload}")
+
+            # Wait for the response/update and process it
+            response = await self.websocket.recv()
+            response = json.loads(response)
+            print(f"Response => {response}")
+            await asyncio.sleep(0.2)
+
+    async def run(self):
+        """Typical client connection"""
+        cache_data = await cache.load()
+
+        # TODO => Fix temp nickname
+        if not cache_data["nickname"]:
+            cache_data["nickname"] = input("Enter a nickname: ")
+
+        async with websockets.connect("ws://localhost:8000/") as self.websocket:
+            # Send the first data to initialize the connection
+            self.payload = await self._hello(cache_data)
+            # Now play the game
+            await self.play(self.payload)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    client = Client()
+    asyncio.run(client.run())
